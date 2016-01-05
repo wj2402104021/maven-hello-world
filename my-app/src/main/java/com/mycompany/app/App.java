27b714f7a626ca6ac10739140e1a814396dfafa5
wangyw <wangyw@yun70.com.cn>
2016-01-05 11:02:28 +0800

    test

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App
     {
         //System.out.println( "Hello World!" );
 
-        System.out.println("A NullPointerException is a drag...");
+        //System.out.println("A NullPointerException is a drag...");
         Object myObject = null;
         /**
          * Checker prevents this from compiling...
@@ -24,6 +24,6 @@ public class App
          *
          * http://checkerframework.org
          */
-        System.out.println("... but thankfully, Checker has our back: http://checkerframework.org");
+        //System.out.println("... but thankfully, Checker has our back: http://checkerframework.org");
     }
 }
